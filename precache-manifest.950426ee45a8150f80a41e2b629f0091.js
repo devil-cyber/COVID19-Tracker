0a77258589c5a0a285575491bfc3816052eb0a9f
devil-cyber <mani2474695@gmail.com>
2022-01-12 22:53:50 +0530

    Updates

---
+++ precache-manifest.950426ee45a8150f80a41e2b629f0091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ad58ab9954aba8a8658ab9b7dce219",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "617bbe97da26a92f9ac2",
+    "url": "/COVID19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8017f1edf9150ae46208",
+    "url": "/COVID19-Tracker/static/css/main.ae9c052c.chunk.css"
+  },
+  {
+    "revision": "617bbe97da26a92f9ac2",
+    "url": "/COVID19-Tracker/static/js/2.882113d0.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/COVID19-Tracker/static/js/2.882113d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8017f1edf9150ae46208",
+    "url": "/COVID19-Tracker/static/js/main.fe3deb81.chunk.js"
+  },
+  {
+    "revision": "b4c08c8708683a2dc77d",
+    "url": "/COVID19-Tracker/static/js/runtime-main.470ef7e2.js"
+  }
+]);
